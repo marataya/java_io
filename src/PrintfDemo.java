ea795fcae4c4e0be73118ea709797becd31b0989
marataya <marataya@gmail.com>
2020-02-26 15:37:37 +0600

    cleanup

--- src/PrintfDemo.java
+++
@@ -1,8 +0,0 @@
-public class PrintfDemo {
-    public static void main(String[] args) {
-        System.out.println("Here are numeric values in different formats.\n");
-        System.out.printf("Various integer formats: ");
-        System.out.printf("%d %(d %+d %05d\n", 3, -3, 3, 3);
-        System.out.println();
-    }
-}
